4672ae99d1039ae1c6d9b021419ac662576a52d0
g1ts <coder@g1ts.net>
2012-08-29 16:31:53 +0600

    added pipe for charts

--- utils.js
+++ utils.js
@@ -13,3 +13,37 @@ function randomString(l, chars){
 }
 function randomHex(l){return randomString(l,'0123456789abcdef'.split(''))}
 function randomHexBytes(l){return randomHex(l*2)}	// 0x00 - one byte
+
+// pipe for chart
+P = {
+	_l:5,
+	setL:function(l){
+		this._l=l;
+		if(this.a.length>l){this.a.length=l;}
+		this.p=0;
+	},
+	a:[],
+	p:0,
+	_incp:function(p){
+		p++;
+		if(p>=this._l){p=0;}
+		return p;
+	},
+	print:function(){
+		var s='';
+		var p = this.p;
+		for(var i=0;i<this.a.length;i++){
+			s+=this.a[p]+' ';
+			p=this._incp(p);
+		}
+		console.log(s);
+	},
+	push:function(v){
+		if(this.a.length<this._l){
+			this.a.push(v)
+		}else{
+			this.a[this.p]=v;
+			this.p=this._incp(this.p);
+		}
+	}
+}
