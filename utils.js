a0fac6380ed42dc37371073003fa7443d826f1a2
g1ts <coder@g1ts.net>
2012-10-30 10:21:05 +0600

    added genSelector

--- utils.js
+++ utils.js
@@ -71,5 +71,24 @@ window.jsUtils = {
       }
       return _s;
     }
+  },
+  
+  // generate css selector
+  genSelector: function(el) {
+    var names = [];
+    while (el.parentNode){
+      if (el.id){
+        names.unshift('#'+el.id);
+        break;
+      }else{
+        if (el==el.ownerDocument.documentElement) names.unshift(el.tagName);
+        else{
+          for (var c=1,e=el;e.previousElementSibling;e=e.previousElementSibling,c++);
+          names.unshift(el.tagName+":nth-child("+c+")");
+        }
+        el=el.parentNode;
+      }
+    }
+    return names.join(">");
   }
 }
